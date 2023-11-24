d3d0e4de1865f516ced1f60e91b8418e857fd7d5
Arslan 'ArsNotFound' Sakhapov <arslan.sakhapov@yandex.ru>
2023-11-24 16:16:38 +0500

    Config and arg parsing

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"ufahack_2023/internal/config"
+)
 
+type Args struct {
+	ConfigPath string
+}
+
+func processArgs() Args {
+	var a Args
+
+	f := flag.NewFlagSet("Server", flag.ExitOnError)
+	f.StringVar(&a.ConfigPath, "c", "config.yml", "Path to configuration file")
+
+	fu := f.Usage
+	f.Usage = func() {
+		fu()
+		envHelp, _ := config.Description()
+		fmt.Fprintln(f.Output())
+		fmt.Fprintln(f.Output(), envHelp)
+	}
+
+	f.Parse(os.Args[1:])
+	return a
+}
+
+func run() error {
+	args := processArgs()
+
+	cfg, err := config.Load(args.ConfigPath)
+	if err != nil {
+		return err
+	}
+
+	_ = cfg
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(0)
 }
--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,1 +1,36 @@
 package config
+
+import "github.com/ilyakaznacheev/cleanenv"
+
+type ServerConfig struct {
+	Address string `yaml:"address" env:"SERVER_ADDRESS" env-default:"localhost"`
+	Port    string `yaml:"port" env:"SERVER_PORT" env-default:"8080"`
+}
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host" env:"DB_HOST" env-default:"localhost"`
+	Port     string `yaml:"port" env:"DB_PORT" env-default:"5432"`
+	Name     string `yaml:"name" env:"DB_NAME" env-default:"postgres"`
+	User     string `yaml:"user" env:"DB_USER" env-default:"user"`
+	Password string `yaml:"password" env:"DB_PASSWORD" env-default:"secretpass"`
+}
+
+type Config struct {
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
+}
+
+func Load(configPath string) (*Config, error) {
+	cfg := &Config{}
+
+	err := cleanenv.ReadConfig(configPath, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func Description() (string, error) {
+	return cleanenv.GetDescription(&Config{}, nil)
+}
